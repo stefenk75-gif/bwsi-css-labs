a72e220393b26a5315a2f9c1253306b8b4625b24
stefenk75-gif <stefenk75@gmail.com>
2026-03-06 17:07:19 -0600

    added personal description to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Stefen King" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello, my name is Stefen King. I'm a junior at Pass Christian High school in southern Mississippi. I love robotics, football, and music.")
 
 if __name__ == "__main__":
     main()
